49ae2dcbf27dc335eaab3b8d43efbfbc1ad677c8
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-10 12:02:40 +0530

    Flutter Open Store

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:open_store/open_store.dart';
+
+void main() {
+  runApp(const UpenStoreExample());
+}
+
+class UpenStoreExample extends StatelessWidget {
+  const UpenStoreExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            OpenStore.instance.open(
+              appStoreId: '1543803459',
+              androidAppBundleId: 'com.google.android.googlequicksearchbox',
+            );
+          },
+          child: const Text('Open store'),
+        ),
+      ),
+    );
+  }
+}
